16c74cd36e51f65ee918e5187aaf3d5c7d59f733
Divyansh Verma <69747865+DivyanshVerma217@users.noreply.github.com>
2021-06-11 23:04:10 +0530

    Add files via upload

--- src/Recommended.js
+++ src/Recommended.js
@@ -60,7 +60,60 @@ function Recommended() {
                 views = "8.5M Views"
                 channel = {<a href = "https://www.youtube.com/channel/UCk3k1R9FIm8Ogsvn84SJKGg">DISNEY PLUS HOTSTAR PREMIUM</a>}
                 
-           />
+           /> <VideoCard
+           image = "https://i.ytimg.com/an_webp/vqyIYTVFNck/mqdefault_6s.webp?du=3000&sqp=CI_HjYYG&rs=AOn4CLDsGHIrq5b4F0P9_tO8komWWrEsmg"
+           title = {<a href = "https://www.youtube.com/watch?v=vqyIYTVFNck" >FARAAR|Latest Punjabi Song</a>}
+           timestamp = '4 day ago'
+           channelImage = "https://yt3.ggpht.com/ytc/AAUvwnh5kAOkyTTcyFahoJLRRBSSdqYuZ-jDRt5MF2uqAA=s68-c-k-c0x00ffffff-no-rj"
+           views = "5M Views"
+           channel = {<a href = "https://www.youtube.com/channel/UCsaXTlOmt0o9aGeC_n_r8VQ">RUN UP RECORDS</a>}
+
+      />
+       <VideoCard
+           image = "https://i.ytimg.com/an_webp/R7sCbToxT7E/mqdefault_6s.webp?du=3000&sqp=CIPMjYYG&rs=AOn4CLBppwOE4GCXBmRpG2Jb5KSYVZ4LlQ"
+           title = {<a href = "https://www.youtube.com/c/TechnicalGuruji" >Tech Talks #1466 - BGMI 18 June Official, CoWIN Hacked?, Realme Laptop launch, Mi 11 Lite India, M32</a>}
+           timestamp = '1 day ago'
+           channelImage = "https://yt3.ggpht.com/ytc/AAUvwngAg8C9ez6SdBMdgEA-Aud61obXOuY1zsUPkL8Z0w=s176-c-k-c0x00ffffff-no-rj-mo"
+           views = "8M Views"
+           channel = {<a href = "https://www.youtube.com/channel/UCOhHO2ICt0ti9KAh-QHvttQ">TECHNICAL GURUJI</a>}
+           
+      />
+      <VideoCard
+           image = "https://i.ytimg.com/an_webp/MLdXYEWTC1k/mqdefault_6s.webp?du=3000&sqp=CKjTjYYG&rs=AOn4CLCQ3NOVTCAbPwVolZETWTrZMw50-A"
+           title = {<a href = "https://www.youtube.com/watch?v=vqyIYTVFNck" >Bahut Pighle Hain | Zakir khan | Stand-Up Comedy | Sukha poori 6</a>}
+           timestamp = '2 day ago'
+           channelImage = "https://yt3.ggpht.com/ytc/AAUvwnjrRv8PYYcFcNXq2mB3ufVNSOF5N5Z2FV1ljxLhqA=s176-c-k-c0x00ffffff-no-rj-mo"
+           views = "1.4M Views"
+           channel = {<a href = "https://www.youtube.com/channel/UCkS7Vxu4PjM99w0Is6idjcg">ZAKIR KHAN</a>}
+           
+      />
+      <VideoCard
+           image = "https://i.ytimg.com/an_webp/sZJd4epkDu4/mqdefault_6s.webp?du=3000&sqp=CN7rjYYG&rs=AOn4CLCoNNXWFsATkC18HGRwpMoSwtTayA"
+           title = {<a href = "https://www.youtube.com/watch?v=vqyIYTVFNck" >POCO M3 PRO 5G UNBOXING</a>}
+           timestamp = '7 day ago'
+           channelImage = "https://yt3.ggpht.com/ytc/AAUvwngJZm17vNotc0ojgs5rPQ7MC0eI7a2WsOtjw4SzLA=s68-c-k-c0x00ffffff-no-rj"
+           views = "5.8M Views"
+           channel = {<a href = "https://www.youtube.com/c/TechBurner">TECH BURNER</a>}
+           
+      />
+      <VideoCard
+           image = "https://i.ytimg.com/an_webp/wGKb3oUo8go/mqdefault_6s.webp?du=3000&sqp=CJDUjYYG&rs=AOn4CLBuveSj-kHfB4z76iNmq_9oyyQTOA"
+           title = {<a href = "https://www.youtube.com/watch?v=vqyIYTVFNck" >WHY iphone's features always "LATE</a>}
+           timestamp = '9 day ago'
+           channelImage = "https://yt3.ggpht.com/ytc/AAUvwngW9TQgw7E7NqS3Qzd3Up3tjUzkpvMXPWAhYf3LaQ=s68-c-k-c0x00ffffff-no-rj"
+           views = "2.4M Views"
+           channel = {<a href = "https://www.youtube.com/c/mkbhd">MARQUES BROWNLEE</a>}
+           
+      />
+      <VideoCard
+           image = "https://i.ytimg.com/an_webp/bneA0_Sy8PI/mqdefault_6s.webp?du=3000&sqp=CNrMjYYG&rs=AOn4CLAkerL5Ws9we02H34vS4K3k3B6fZw"
+           title = {<a href = "https://www.youtube.com/watch?v=bneA0_Sy8PI" >Loki Trailer | Loki's time has come | Marvel Studios'</a>}
+           timestamp = '15 day ago'
+           channelImage = "https://yt3.ggpht.com/ytc/AAUvwngRLY8gKdFPOlRX1PGNR1kxnMPLhDkFAP5Jlu0doQ=s68-c-k-c0x00ffffff-no-rj"
+           views = "8.5M Views"
+           channel = {<a href = "https://www.youtube.com/channel/UCk3k1R9FIm8Ogsvn84SJKGg">DISNEY PLUS HOTSTAR PREMIUM</a>}
+           
+      />
            
         </div>
             
